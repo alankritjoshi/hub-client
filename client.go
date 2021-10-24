aede6fdd3336c6908c1ddbe43f6db7a08efcec0a
Alankrit Joshi <alankritjoshi@gmail.com>
2021-10-24 15:04:10 -0700

    Added incoming handler

--- client.go
+++ client.go
@@ -20,6 +20,18 @@ func outgoingRoutine(outC chan<- string) {
 	}
 }
 
+func incomingRoutine(inC chan<- string, c net.Conn) {
+	connReader := bufio.NewReader(c)
+	for {
+		i, err := connReader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Incoming error: %v", err)
+			return
+		}
+		inC <- i
+	}
+}
+
 func main() {
 	arguments := os.Args
 	if len(arguments) == 1 {
@@ -37,11 +49,16 @@ func main() {
 	outgoing := make(chan string)
 	go outgoingRoutine(outgoing)
 
+	incoming := make(chan string)
+	go incomingRoutine(incoming, conn)
+
 	for {
 		select {
 		case out := <-outgoing:
 			fmt.Print(">> ")
 			fmt.Fprintf(conn, out+"\n")
+		case in := <-incoming:
+			fmt.Print("\n<- " + in + ">> ")
 		}
 	}
 
